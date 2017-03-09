e47b8c5ed63a3f7e9d48b883659064782cf311c2
LiebeStar <liebe_821227@yahoo.com.tw>
2017-03-09 12:18:03 +0800

    Add function reading csv file

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -6,3 +6,10 @@ Created on Thu Mar  9 11:35:15 2017
 @author: liebe
 """
 
+import sys
+import csv
+
+f = open('train.csv', 'r', encoding="big5")
+for row in csv.reader(f):
+    print(row)
+f.close()
